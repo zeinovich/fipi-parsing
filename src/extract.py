557b7bf8c4855f99fd0e6d703af288c4bb720f8e
zeinovich <nikita.zinovich@gmail.com>
2024-12-10 16:16:00 +0300

    Fixed extraction bug + new clustering plot

--- src/extract.py
+++ src/extract.py
@@ -1,32 +1,52 @@
 import requests
-from src.constants import QUESTIONS_URL, PAGES_COUNT, MULTI_CHOICE_TASK_TYPE
+import json
+
+from tqdm import tqdm
+
+from src.constants import QUESTIONS_URL, PAGES_COUNT
 from src.task_extractor import TaskExtractor
 
+
 def main():
-    for page in range(1, PAGES_COUNT + 1):
+    tasks = []
+
+    for page in tqdm(range(1, PAGES_COUNT + 1)):
         page_content = process_page(page)
-        parse_page(page_content)
+        page_tasks = parse_page(page_content)
+
+        tasks.extend(page_tasks)
+
+    with open("./data/raw/tasks.json", "w") as f:
+        json.dump(
+            tasks,
+            f,
+            indent=4,
+            ensure_ascii=False,
+        )
+
 
 def process_page(page: int):
     url = QUESTIONS_URL + f"&page={page}"
     response = requests.get(url)
     return response.text
 
+
 def parse_page(page_content: str):
     extractor = TaskExtractor(page_content)
     tasks = extractor.extract_tasks()
+    task_list = []
+
     for task in tasks:
         task_data = extractor.extract_task(task)
-        print(f'=== NEW TASK ===')
-        print(f'id: {task_data.id}')
-        print(f'guid: {task_data.guid}')
-        print(f'task_info: {task_data.task_info}')
-        print(f'content: {task_data.content}')
-        print(f'answer: {task_data.answer}')
-        print(f'answer_type: {task_data.answer_type}')
-        print(f'tags: {task_data.tags}')
-        print(f'=== END OF TASK ===')
+
+        if task_data is None:
+            print("Task can't be extracted")
+            continue
+
+        task_list.append(task_data.model_dump())
+
+    return task_list
+
 
 if __name__ == "__main__":
     main()
-
--- src/task_extractor.py
+++ src/task_extractor.py
@@ -1,45 +1,67 @@
-from bs4 import BeautifulSoup
+from typing import List, Optional
+
 from pydantic import BaseModel
-from typing import List
+from bs4 import BeautifulSoup
 from bs4.element import PageElement
+
 from src.constants import MULTI_CHOICE_TASK_TYPE
 from src.multi_choice_solver import solve_multi_choice_task
+
 
 class Task(BaseModel):
     id: str
     guid: str
     task_info: str
     content: str
-    answer: str
+    answer: Optional[str]
     answer_type: str
     tags: List[str]
+
 
 class TaskExtractor:
     def __init__(self, html_content: str):
         self.html_content = html_content
-        self.soup = BeautifulSoup(self.html_content, 'html.parser')
+        self.soup = BeautifulSoup(self.html_content, "html.parser")
 
     def extract_tasks(self):
-        tasks = self.soup.find_all('div', {'class': 'qblock'})
+        tasks = self.soup.find_all("div", {"class": "qblock"})
         return tasks
-    
+
     def extract_task(self, task: PageElement):
-        task_id = task['id']
-        task_info = f'i{task_id[1:]}'
-        task_guid = task.find('input', {'name': 'guid'})['value']
+        task_id = task.get("id", None)
+
+        if task_id is None:
+            return None
+
+        task_info = f"i{task_id[1:]}"
+        task_guid = task.find("input", {"name": "guid"})["value"]
         task_content = ""
-        task_contents = task.find_all('p')
+        task_contents = task.find_all("p")
+
         for i, elem in enumerate(task_contents):
             if i != len(task_contents) - 1:
                 task_content = task_content.strip()
                 task_content += "\n"
             task_content += elem.text
-        info = self.soup.find('div', {'id': task_info})
+
+        info = self.soup.find("div", {"id": task_info})
         tags = []
-        for td in info.find_all('td', {'class': 'param-row'}):
+
+        for td in info.find_all("td", {"class": "param-row"}):
             tags.append(td.text)
-        answer_type = info.find_all('td')[-1].text
-        answer = "" 
-        if answer_type == MULTI_CHOICE_TASK_TYPE:
-            answer = solve_multi_choice_task(task_guid)
-        return Task(id=task_id, guid=task_guid, task_info=task_info, content=task_content, answer=answer, answer_type=answer_type, tags=tags)
+
+        answer_type = info.find_all("td")[-1].text
+        answer = ""
+
+        # if answer_type == MULTI_CHOICE_TASK_TYPE:
+        #     answer = solve_multi_choice_task(task_guid)
+
+        return Task(
+            id=task_id,
+            guid=task_guid,
+            task_info=task_info,
+            content=task_content,
+            answer=answer,
+            answer_type=answer_type,
+            tags=tags,
+        )
